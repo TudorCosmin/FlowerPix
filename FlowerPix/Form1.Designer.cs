09840ba45483bae5fe32ebc037424d3db9d34b76
Tudor Cosmin Oanea <51297266+TudorCosmin@users.noreply.github.com>
2021-02-28 20:40:52 +0200

    Add files via upload

---
+++ FlowerPix/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace din_flori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.poza = new System.Windows.Forms.PictureBox();
+            this.b_selectare = new System.Windows.Forms.Button();
+            this.b_exit = new System.Windows.Forms.Button();
+            this.b_conversie = new System.Windows.Forms.Button();
+            this.b_salvare = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown_nrmultiplicare = new System.Windows.Forms.NumericUpDown();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.poza)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_nrmultiplicare)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // poza
+            // 
+            this.poza.Location = new System.Drawing.Point(12, 12);
+            this.poza.Name = "poza";
+            this.poza.Size = new System.Drawing.Size(601, 281);
+            this.poza.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.poza.TabIndex = 0;
+            this.poza.TabStop = false;
+            // 
+            // b_selectare
+            // 
+            this.b_selectare.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_selectare.Location = new System.Drawing.Point(12, 309);
+            this.b_selectare.Name = "b_selectare";
+            this.b_selectare.Size = new System.Drawing.Size(100, 40);
+            this.b_selectare.TabIndex = 1;
+            this.b_selectare.Text = "Selectare imagine";
+            this.b_selectare.UseVisualStyleBackColor = true;
+            this.b_selectare.Click += new System.EventHandler(this.b_selectare_Click);
+            // 
+            // b_exit
+            // 
+            this.b_exit.Location = new System.Drawing.Point(513, 309);
+            this.b_exit.Name = "b_exit";
+            this.b_exit.Size = new System.Drawing.Size(100, 40);
+            this.b_exit.TabIndex = 2;
+            this.b_exit.Text = "Exit";
+            this.b_exit.UseVisualStyleBackColor = true;
+            this.b_exit.Click += new System.EventHandler(this.b_exit_Click);
+            // 
+            // b_conversie
+            // 
+            this.b_conversie.Enabled = false;
+            this.b_conversie.Location = new System.Drawing.Point(301, 309);
+            this.b_conversie.Name = "b_conversie";
+            this.b_conversie.Size = new System.Drawing.Size(100, 40);
+            this.b_conversie.TabIndex = 3;
+            this.b_conversie.Text = "Conversie";
+            this.b_conversie.UseVisualStyleBackColor = true;
+            this.b_conversie.Click += new System.EventHandler(this.b_conversie_Click);
+            // 
+            // b_salvare
+            // 
+            this.b_salvare.Enabled = false;
+            this.b_salvare.Location = new System.Drawing.Point(407, 309);
+            this.b_salvare.Name = "b_salvare";
+            this.b_salvare.Size = new System.Drawing.Size(100, 40);
+            this.b_salvare.TabIndex = 4;
+            this.b_salvare.Text = "Salvare imagine";
+            this.b_salvare.UseVisualStyleBackColor = true;
+            this.b_salvare.Click += new System.EventHandler(this.b_salvare_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Numar de flori pe latime:";
+            // 
+            // numericUpDown_nrmultiplicare
+            // 
+            this.numericUpDown_nrmultiplicare.Location = new System.Drawing.Point(240, 321);
+            this.numericUpDown_nrmultiplicare.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDown_nrmultiplicare.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown_nrmultiplicare.Name = "numericUpDown_nrmultiplicare";
+            this.numericUpDown_nrmultiplicare.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown_nrmultiplicare.TabIndex = 7;
+            this.numericUpDown_nrmultiplicare.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown_nrmultiplicare.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(301, 309);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 40);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 8;
+            this.progressBar.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 356);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.numericUpDown_nrmultiplicare);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b_salvare);
+            this.Controls.Add(this.b_conversie);
+            this.Controls.Add(this.b_exit);
+            this.Controls.Add(this.b_selectare);
+            this.Controls.Add(this.poza);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FlowerPix - Proiect de atestat - Oanea Tudor Cosmin";
+            ((System.ComponentModel.ISupportInitialize)(this.poza)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_nrmultiplicare)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox poza;
+        private System.Windows.Forms.Button b_selectare;
+        private System.Windows.Forms.Button b_exit;
+        private System.Windows.Forms.Button b_conversie;
+        private System.Windows.Forms.Button b_salvare;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown_nrmultiplicare;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ FlowerPix/Form1.cs
@@ -0,0 +1,258 @@
+﻿using din_flori.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace din_flori
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imagine;
+        string nume_initial;
+        double[,] medie = new double[4,503];
+        int latime, inaltime, latimeNoua, inaltimeNoua;
+        DirectoryInfo folder_poze_flori = new DirectoryInfo(@"..\..\Resurse\colectie");
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar.Visible = false;
+            CalculeazaMediaFlori();
+        }
+
+        private void b_selectare_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "Image files (*.jpg, *.png, *.jpeg) | *.jpg; *.png; *jpeg";
+
+            if (DialogResult.OK == ofile.ShowDialog())
+            {
+                // incarc imaginea
+                Image img = Image.FromFile(ofile.FileName);
+                imagine = new Bitmap(img);
+                this.poza.Image = imagine;
+                latime = imagine.Width;
+                inaltime = imagine.Height;
+                b_salvare.Enabled = false;
+                b_conversie.Enabled = true;
+
+
+                // retin numele initial
+
+                // iau path ul si elimin tot ce e inaintea numelui
+                string imagePath = ofile.FileName.ToString();
+                nume_initial = imagePath.ToString();
+                nume_initial = nume_initial.Substring(nume_initial.LastIndexOf("\\"));
+                nume_initial = nume_initial.Remove(0, 1);
+
+                // elimin .jpg de la final
+                int poz  =nume_initial.IndexOf(".");
+                nume_initial = nume_initial.Remove(poz, 4);
+            }
+        }
+
+        private void b_conversie_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown_nrmultiplicare.Value >= 100)
+            {
+                if(MessageBox.Show("Cu cat numarul de poze cu flori pe latime " +
+                    "este mai mare, cu atat mai mult timp va dura conversia.\n" +
+                    "Esti sigur ca vrei sa faci conversia cu " + numericUpDown_nrmultiplicare.Value +
+                    " flori pe latime?" , "Atentie!" , MessageBoxButtons.YesNo , MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+
+                    Redimensionare();
+
+                    InlocuireCuFlori();
+
+                    this.poza.Image = imagine;
+                    b_exit.Enabled = true;
+                    b_salvare.Enabled = true;
+                }
+            }
+            else
+            {
+                Redimensionare();
+
+                InlocuireCuFlori();
+
+                this.poza.Image = imagine;
+                b_exit.Enabled = true;
+                b_salvare.Enabled = true;
+            }
+            
+        }
+
+        private void b_salvare_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Image files (*.jpg, *.png, *.jpeg) | *.jpg; *.png; *jpeg";
+            dialog.FileName = nume_initial +  "_dinflori";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imagine.Save(dialog.FileName, ImageFormat.Jpeg);
+                b_salvare.Enabled = false;
+            }
+        }
+
+        void CalculeazaMediaFlori()
+        {
+            int k, sumaR, sumaG, sumaB, i, j, latime_curenta, inaltime_curenta, d;
+            double nr_pix_floare;
+            k = 1;
+            
+            for(d = 1; d <= 500; d++)
+            {
+                Image fl = Image.FromFile(folder_poze_flori + @"\" + d + ".png");
+                Bitmap floare = new Bitmap(fl);
+                latime_curenta = floare.Width;
+                inaltime_curenta = floare.Height;
+                nr_pix_floare = latime_curenta * inaltime_curenta; // cati pixeli sunt in floarea curenta
+
+                sumaR = sumaG = sumaB = 0;
+                for (i = 0; i < latime_curenta; i++)
+                    for(j = 0; j < inaltime_curenta; j++)
+                    {
+                        sumaR += floare.GetPixel(i, j).R;
+                        sumaG += floare.GetPixel(i, j).G;
+                        sumaB += floare.GetPixel(i, j).B;
+                    }
+
+                medie[0, k] = sumaR / nr_pix_floare;
+                medie[1, k] = sumaG / nr_pix_floare;
+                medie[2, k] = sumaB / nr_pix_floare;
+                k++;
+            }
+        }
+
+        Bitmap ResizeBitmap(Bitmap bmp, int width, int height) // de pe net :D
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+
+            return result;
+        }
+
+        void Redimensionare()
+        {
+            int numar_multiplicare;
+            numar_multiplicare = Convert.ToInt32(Math.Round(numericUpDown_nrmultiplicare.Value, 0));
+
+            // fac regula de 3 simpla:
+            // latime   ..........  40 * numar_multiplicare
+            // inaltime ..........  x
+            latimeNoua = 40 * numar_multiplicare;
+            inaltimeNoua = (((inaltime * latimeNoua) / latime) / 28) * 28;
+            // fac / 28 * 28 ca sa nu imi ramana spatiu gol, chiar daca modific putin raportul pozei
+
+            imagine = ResizeBitmap(imagine, latimeNoua, inaltimeNoua);
+
+        }
+
+        double Distanta(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double dist;
+            double p = 10.0; // teoretic cu cat e mai mare p cu atat mai buna e aproximatia la Minkovski
+
+            // distanta euclidiana:
+            dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+            // distanta Manhattan:
+            //dist = Math.Abs(x2 - x1) + Math.Abs(y2 - y1) + Math.Abs(z2 - z1);
+
+            // distanta Minkovski
+            //dist = Math.Pow(Math.Pow(Math.Abs(x2 - x1), p) + Math.Pow(Math.Abs(y2 - y1), p) + Math.Pow(Math.Abs(z2 - z1), p), 1 / p);
+
+            return dist;
+        }
+
+        void InlocuireCuFlori()
+        {
+            progressBar.Value = 0;
+            progressBar.Visible = true;
+
+            int i, j, d, k, poz;
+            double medieR, medieG, medieB, dist_minima, dist_curenta, sumaR, sumaG, sumaB;
+
+            //calculez dimensiunile primei flori ca sa am punct de reper pentru fiecare fereastra
+            Image fl1 = Image.FromFile(folder_poze_flori + @"\" + 1 + ".png");
+            Bitmap floare1 = new Bitmap(fl1);
+
+            int latime_fereastra, inaltime_fereastra, nr_pix_floare;
+            latime_fereastra = floare1.Width;//40
+            inaltime_fereastra = floare1.Height;//28
+            nr_pix_floare = latime_fereastra * inaltime_fereastra; // cati pixeli sunt intr-o floare
+
+            progressBar.Maximum = latimeNoua;
+
+            //inlocuirea efectiva
+            for (i = 0; i < latimeNoua; i += latime_fereastra)
+            {
+                progressBar.Value = i;
+                for (j = 0; j < inaltimeNoua; j += inaltime_fereastra)
+                {
+                    //fac media ferestrei curente
+                    sumaR = sumaG = sumaB = 0;
+                    for (k = 0; k < latime_fereastra; k++)
+                        for (d = 0; d < inaltime_fereastra; d++)
+                        {
+                            sumaR += imagine.GetPixel(i + k, j + d).R;
+                            sumaG += imagine.GetPixel(i + k, j + d).G;
+                            sumaB += imagine.GetPixel(i + k, j + d).B;
+                        }
+                    medieR = sumaR / nr_pix_floare;
+                    medieG = sumaG / nr_pix_floare;
+                    medieB = sumaB / nr_pix_floare;
+
+                    //gasesc cea mai apropiata floare de fereastra curenta
+                    dist_minima = Distanta(medieR, medieG, medieB, medie[0, 1], medie[1, 1], medie[2, 1]);
+                    poz = 1;
+                    for (k = 2; k <= 500; k++)
+                    {
+                        dist_curenta = Distanta(medieR, medieG, medieB, medie[0, k], medie[1, k], medie[2, k]);
+                        if (dist_curenta < dist_minima)
+                        {
+                            dist_minima = dist_curenta;
+                            poz = k;
+                        }
+                    }
+
+                    //copiez floarea de la pozitia poz in fereastra curenta
+                    Image fl = Image.FromFile(folder_poze_flori + @"\" + poz + ".png");
+                    Bitmap floare = new Bitmap(fl);
+                    Color culoare = new Color();
+                    for (k = 0; k < latime_fereastra; k++)
+                        for (d = 0; d < inaltime_fereastra; d++)
+                        {
+                            culoare = floare.GetPixel(k, d);
+                            imagine.SetPixel(i + k, j + d, culoare);
+                        }
+                }
+            }
+
+            progressBar.Visible = false;
+        }
+
+        private void b_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
